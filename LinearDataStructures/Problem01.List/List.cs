423fcc799d0e5a8cfa89cc2860d6754d9be10d02
danail2003 <ddobrev20@outlook.com>
2021-09-15 20:21:31 +0300

    list implemented

--- LinearDataStructures/Problem01.List/List.cs
+++ LinearDataStructures/Problem01.List/List.cs
@@ -7,7 +7,8 @@
     public class List<T> : IAbstractList<T>
     {
         private const int DEFAULT_CAPACITY = 4;
-        private readonly T[] items;
+        private T[] items;
+        private int size;
 
         public List()
             : this(DEFAULT_CAPACITY) {
@@ -15,60 +16,145 @@
 
         public List(int capacity)
         {
-            throw new NotImplementedException();
+            this.items = new T[capacity];
         }
 
         public T this[int index]
         {
             get
             {
-                throw new NotImplementedException();
+                this.ValidateIndex(index);
+
+                return this.items[index];
             }
             set
             {
-                throw new NotImplementedException();
+                this.ValidateIndex(index);
+
+                this.items[index] = value;
             }
         }
 
-        public int Count { get; private set; }
+        public int Count => this.size;
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            if (this.size == this.items.Length)
+            {
+                this.items = this.Resize();
+            }
+
+            this.items[size++] = item;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                if (this.items[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
 
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            int index = -1;
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                if (this.items[i].Equals(item))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
         }
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            this.ValidateIndex(index);
+
+            if (this.Count == this.items.Length)
+            {
+                this.items = this.Resize();
+            }
+
+            for (int i = this.Count; i > index; i--)
+            {
+                this.items[i] = this.items[i - 1];
+            }
+
+            this.items[index] = item;
+            this.size++;
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            bool isExist = false;
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                if (this.items[i].Equals(item))
+                {
+                    for (int j = i; j < this.items.Length - 1; j++)
+                    {
+                        this.items[j] = this.items[j + 1];
+                    }
+
+                    this.size--;
+
+                    isExist = true;
+                }
+            }
+
+            return isExist;
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            this.ValidateIndex(index);
+
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+
+            this.items[this.Count - 1] = default;
+            this.size--;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this.items[i];
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator() 
-            => throw new NotImplementedException();
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private T[] Resize()
+        {
+            T[] newArray = new T[this.items.Length * 2];
+            Array.Copy(this.items, newArray, this.items.Length);
+
+            return newArray;
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException(nameof(index));
+            }
+        }
     }
 }
