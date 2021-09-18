876d4337b791f5e50b1e07c5e64c04dd1ed9155c
danail2003 <ddobrev20@outlook.com>
2021-09-18 20:33:48 +0300

    BP implemented

--- LinearDataStructures/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ LinearDataStructures/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -1,13 +1,52 @@
 ﻿namespace Problem04.BalancedParentheses
 {
-    using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class BalancedParenthesesSolve : ISolvable
     {
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+            Stack<char> symbols = new Stack<char>();
+
+            for (int i = 0; i < parentheses.Length; i++)
+            {
+                if (parentheses[i] == '{')
+                {
+                    symbols.Push(parentheses[i]);
+                }
+                else if (parentheses[i] =='[')
+                {
+                    symbols.Push(parentheses[i]);
+                }
+                else if (parentheses[i] == '(')
+                {
+                    symbols.Push(parentheses[i]);
+                }
+                else if (parentheses[i] == '}')
+                {
+                    if (!symbols.Any() || symbols.Pop() != '{')
+                    {
+                        return false;
+                    }
+                }
+                else if (parentheses[i] == ']')
+                {
+                    if (!symbols.Any() || symbols.Pop() != '[')
+                    {
+                        return false;
+                    }
+                }
+                else if (parentheses[i] == ')')
+                {
+                    if (!symbols.Any() || symbols.Pop() != '(')
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
