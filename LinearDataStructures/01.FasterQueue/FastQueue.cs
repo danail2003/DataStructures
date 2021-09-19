34b0dbedd5733ba1b993f00c92b59d3823e73bc6
danail2003 <ddobrev20@outlook.com>
2021-09-19 10:21:00 +0300

    fast queue is implemented

--- LinearDataStructures/01.FasterQueue/FastQueue.cs
+++ LinearDataStructures/01.FasterQueue/FastQueue.cs
@@ -7,36 +7,86 @@
     public class FastQueue<T> : IAbstractQueue<T>
     {
         private Node<T> _head;
+        private Node<T> _tail;
+
         public int Count { get; private set; }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            Node<T> current = this._head;
+
+            while (current != null)
+            {
+                if (current.Item.Equals(item))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (this._head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            Node<T> current = this._head;
+            T oldValue = current.Item;
+            this._head = current.Next;
+
+            this.Count--;
+
+            return oldValue;
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            Node<T> newItem = new Node<T>
+            {
+                Item = item,
+                Next = null
+            };
+
+            if (this._head == null)
+            {
+                this._head = this._tail = newItem;
+            }
+            else
+            {
+                this._tail.Next = newItem;
+                this._tail = this._tail.Next;
+            }
+
+            this.Count++;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if (this._head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this._head.Item;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            Node<T> current = this._head;
+
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+            => this.GetEnumerator();
     }
 }
