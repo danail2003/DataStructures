de77af6c699c597931556664824c6ad25ab5d31d
danail2003 <ddobrev20@outlook.com>
2021-09-19 12:03:31 +0300

    doubly linked list is implemented without add last method

--- LinearDataStructures/02.DoublyLinkedList/DoublyLinkedList.cs
+++ LinearDataStructures/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -7,12 +7,32 @@
     public class DoublyLinkedList<T> : IAbstractLinkedList<T>
     {
         private Node<T> head;
+        private Node<T> tail;
 
         public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            Node<T> newNode = new Node<T>
+            {
+                Item = item,
+                Next = null,
+            };
+
+            if (this.head == null)
+            {
+                this.head = this.tail = newNode;
+            }
+            else
+            {
+                Node<T> oldHead = this.head;
+
+                oldHead.Previous = newNode;
+                this.head = newNode;
+                this.head.Next = oldHead;
+            }
+
+            this.Count++;
         }
 
         public void AddLast(T item)
@@ -22,32 +42,90 @@
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            if (this.head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.head.Item;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            if (this.head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            Node<T> current = this.head;
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            return current.Item;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            if (this.head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            T oldValue = this.head.Item;
+            this.head = this.head.Next;
+
+            this.Count--;
+
+            return oldValue;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            if (this.head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (this.Count == 1)
+            {
+                this.Count--;
+
+                T oldValue = this.head.Item;
+                this.head = default;
+
+                return oldValue;
+            }
+
+            Node<T> current = this.head;
+
+            for (int i = 0; i < this.Count - 2; i++)
+            {
+                current = current.Next;
+            }
+
+            T lastElement = current.Next.Item;
+            current.Next = default;
+
+            this.Count--;
+
+            return lastElement;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            Node<T> current = this.head;
+
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+            => this.GetEnumerator();
     }
 }
