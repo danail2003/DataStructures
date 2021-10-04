4bc049156f192fc6879f4e41cb5636f2b1413a34
danail2003 <ddobrev20@outlook.com>
2021-10-04 20:14:38 +0300

    implemented some methods of BST

--- BST & Heaps/01.BSTOperations/BinarySearchTree.cs
+++ BST & Heaps/01.BSTOperations/BinarySearchTree.cs
@@ -12,31 +12,82 @@
 
         public BinarySearchTree(Node<T> root)
         {
-            // TODO: Create copy from root
+            this.Copy(root);
         }
 
         public Node<T> Root { get; private set; }
 
         public int Count => throw new NotImplementedException();
 
+        public Node<T> LeftChild { get; private set; }
+
+        public Node<T> RightChild { get; private set; }
+
+        public T Value => this.Root.Value;
+
         public bool Contains(T element)
         {
-            throw new NotImplementedException();
+            Node<T> current = this.Root;
+
+            while (current != null)
+            {
+                if (this.IsLess(element, current.Value))
+                {
+                    current = current.LeftChild;
+                }
+                else if (this.IsGreater(element, current.Value))
+                {
+                    current = current.RightChild;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public void Insert(T element)
         {
-            throw new NotImplementedException();
+            Node<T> toInsert = new Node<T>(element, null, null);
+
+            if (this.Root == null)
+            {
+                this.Root = toInsert;
+            }
+            else
+            {
+                this.InsertElementDfs(this.Root, null, toInsert);
+            }
         }
 
         public IAbstractBinarySearchTree<T> Search(T element)
         {
-            throw new NotImplementedException();
+            Node<T> current = this.Root;
+
+            while (current != null)
+            {
+                if (this.IsLess(element, current.Value))
+                {
+                    current = current.LeftChild;
+                }
+                else if (this.IsGreater(element, current.Value))
+                {
+                    current = current.RightChild;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return new BinarySearchTree<T>(current);
         }
 
         public void EachInOrder(Action<T> action)
         {
-            throw new NotImplementedException();
+            this.EachInOrderDfs(this.Root, action);
         }
 
         public List<T> Range(T lower, T upper)
@@ -46,7 +97,10 @@
 
         public void DeleteMin()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
         }
 
         public void DeleteMax()
@@ -57,6 +111,77 @@
         public int GetRank(T element)
         {
             throw new NotImplementedException();
+        }
+
+        private void InsertElementDfs(Node<T> current, Node<T> previous, Node<T> toInsert)
+        {
+            if (current == null && this.IsLess(toInsert.Value, previous.Value))
+            {
+                previous.LeftChild = toInsert;
+
+                if (this.LeftChild == null)
+                {
+                    this.LeftChild = toInsert;
+                }
+
+                return;
+            }
+
+            if (current == null && this.IsGreater(toInsert.Value, previous.Value))
+            {
+                previous.RightChild = toInsert;
+
+                if (this.RightChild == null)
+                {
+                    this.RightChild = toInsert;
+                }
+
+                return;
+            }
+
+            if (this.IsLess(toInsert.Value, current.Value))
+            {
+                this.InsertElementDfs(current.LeftChild, current, toInsert);
+            }
+            else if (this.IsGreater(toInsert.Value, current.Value))
+            {
+                this.InsertElementDfs(current.RightChild, current, toInsert);
+            }
+        }
+
+        private bool IsLess(T first, T second)
+        {
+            return first.CompareTo(second) < 0;
+        }
+
+        private bool IsGreater(T first, T second)
+        {
+            return first.CompareTo(second) > 0;
+        }
+
+        private bool AreEqual(T first, T second)
+        {
+            return first.CompareTo(second) == 0;
+        }
+
+        private void EachInOrderDfs(Node<T> current, Action<T> action)
+        {
+            if (current != null)
+            {
+                this.EachInOrderDfs(current.LeftChild, action);
+                action.Invoke(current.Value);
+                this.EachInOrderDfs(current.RightChild, action);
+            }
+        }
+
+        private void Copy(Node<T> current)
+        {
+            if (current != null)
+            {
+                this.Insert(current.Value);
+                this.Copy(current.LeftChild);
+                this.Copy(current.RightChild);
+            }
         }
     }
 }
--- BST & Heaps/01.BSTOperations/Node.cs
+++ BST & Heaps/01.BSTOperations/Node.cs
@@ -2,17 +2,17 @@
 {
     public class Node<T>
     {
-        public T Value { get; set; }
-
-        public Node<T> LeftChild { get; set; }
-
-        public Node<T> RightChild { get; set; }
-
         public Node(T value, Node<T> leftChild, Node<T> rightChild)
         {
             this.Value = value;
             this.LeftChild = leftChild;
             this.RightChild = rightChild;
         }
+
+        public T Value { get; set; }
+
+        public Node<T> LeftChild { get; set; }
+
+        public Node<T> RightChild { get; set; }
     }
 }
